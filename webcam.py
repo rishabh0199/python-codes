21b1acbea818ec0284d97a609f12e93cf241f573
rishabh0199 <rishabh01991043@bbdu.ac.in>
2021-03-03 00:21:51 +0530

    listhomework🤪

--- webcam.py
+++ webcam.py
@@ -5,6 +5,8 @@ webcam = cv2.VideoCapture(0)
 while True:
     state, img = webcam.read()
     if state:
+        print(state)
+        print(img)
         cv2.imshow("mycam",img)
         if cv2.waitKey(1) == ord('r'):
             break
